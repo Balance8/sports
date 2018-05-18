d45d09da1ce8157f10dd4d2a85695e87d0f7d334
Michael Slocum <mslocum819@gmail.com>
2018-05-18 11:05:33 -0700

    Refactor

--- src/index.js
+++ src/index.js
@@ -9,6 +9,33 @@ const App = () => (
   </div>
 );
 
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>6/5 @ Evergreens</li>
+      <li>6/8 vs Kickers</li>
+      <li>6/14 @ United</li>
+    </ul>
+  </div>
+);
+
+const Home = () => (
+  <div>
+    <h1>Hello and welcome</h1>
+  </div>
+);
+
+// The Roster component matches one of two different routes
+// depending on the full pathname
+const Roster = () => (
+  <Switch>
+    <Route exact path="/roster" component={FullRoster} />
+    <Route path="/roster/:number" component={Player} />
+  </Switch>
+);
+
+
+
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
 // and /schedule routes will match any pathname that starts
@@ -65,30 +92,6 @@ const PlayerAPI = {
   }
 };
 
-const Schedule = () => (
-  <div>
-    <ul>
-      <li>6/5 @ Evergreens</li>
-      <li>6/8 vs Kickers</li>
-      <li>6/14 @ United</li>
-    </ul>
-  </div>
-);
-
-// The FullRoster iterates over all of the players and creates
-// a link to their profile page.
-const FullRoster = () => (
-  <div>
-    <ul>
-      {PlayerAPI.all().map(p => (
-        <li key={p.number}>
-          <Link to={`/roster/${p.number}`}>{p.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
 // The Player looks up the player using the number parsed from
 // the URL's pathname. If no player is found with the given
 // number, then a "player not found" message is displayed.
@@ -108,14 +111,20 @@ const Player = props => {
   );
 };
 
-// The Roster component matches one of two different routes
-// depending on the full pathname
-const Roster = () => (
-  <Switch>
-    <Route exact path="/roster" component={FullRoster} />
-    <Route path="/roster/:number" component={Player} />
-  </Switch>
+// The FullRoster iterates over all of the players and creates
+// a link to their profile page.
+const FullRoster = () => (
+  <div>
+    <ul>
+      {PlayerAPI.all().map(p => (
+        <li key={p.number}>
+          <Link to={`/roster/${p.number}`}>{p.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
+
 
 // We need to wrap all our routing components in <BrowserRouter />
 ReactDOM.render(
